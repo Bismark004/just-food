9793f2a2356cd870b93a74d021336bebbc48b97b
Bismark Adzai <adzaibismark@gmail.com>
2024-02-16 13:13:59 +0000

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
-// Importing necessary modules from React library
-import { Fragment, useState } from 'react';
-import './App.css';
-import Foods from './Food.js';
+import "./App.css";
+import React, { Fragment, useState } from "react";
+import Foods from "./food";
 export const foodItemsContext = React.createContext();
-
-// Main functional component definition for App
-function App() {
+const App = () => {
   const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
   const [menuItems, setMenuItems] = useState([
-    // Sample menu items with properties like id, name, quantity, description, price, and image
     {
       id: 1,
       name: "Chicken Burger",
       quantity: 40,
       desc: "Fried chicken burger - lettuce, tomato, cheese and mayonnaise",
-      price: "240",
+      price: "24",
       image: "cb.jpg",
     },
-
     {
       id: 2,
       name: "Veg Burger",
       quantity: 30,
       desc: "Plant-based burger — lettuce, tomato, vegan cheese and mayonnaise",
-      price: "220",
+      price: "22",
       image: "vb.jpg",
     },
     {
@@ -31,7 +26,7 @@ function App() {
       name: "Chips",
       quantity: 50,
       desc: "Potato chips fried to perfection",
-      price: "70",
+      price: "7",
       image: "chips.jpg",
     },
     {
@@ -39,56 +34,45 @@ function App() {
       name: "Ice Cream",
       quantity: 30,
       desc: "Ice cream - Vanilla ice cream double scoop",
-      price: "40",
+      price: "4",
       image: "ic.jpg",
     },
-    
   ]);
 
-  
 
-
-  
-  // Rendering the component
   return (
-    <foodItemsContext.Providerrovider value={menuItems}>
+    <foodItemsContext.Provider value={menuItems}>
     <div className="App">
-      {/* Button to toggle between availability check and order food pages */}
-      <button className='toggleButton'
-       onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
-       >
-        {isChooseFoodPage ? "Availability Check" : "Order Food" }
+      <button
+        className="toggleButton"
+        onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
+      >
+        {isChooseFoodPage ? "Availability Check" : "Order Food"}
       </button>
-      
-      {/* Header for the online food shop */}
-      <h3 className="title"> Marc Food Online Shop </h3>
-
-      {/* Conditionally rendering content based on the selected page */}
+      <h3 className="title">Just Food Online Shop</h3>
       {!isChooseFoodPage && (
         <Fragment>
-          {/* Header for the menu availability page */}
-          <h4 className="subTitle"> Menu Availability </h4>
-          
-          {/* List of menu items with their names and quantities */}
+          <h4 className="subTitle">Menu Availability</h4>
           <ul className="ulApp">
-            {menuItems.map((item) => (
-              <li key={item.id} className='liAppItem'>
-                {item.name} - {item.quantity} 
-              </li>
-            ))}
+            {menuItems.map((item) => {
+              return (
+                <li key={item.id} className="liApp">
+                  {item.name} - {item.quantity}
+                </li>
+              );
+            })}
           </ul>
         </Fragment>
       )}
-
       {isChooseFoodPage && (
-        // Rendering the Foods component for ordering food
-        <Foods 
-        foodItems={menuItems} 
+        <Foods
+          foodItems={menuItems}
+        
         ></Foods>
       )}
     </div>
-    </foodItemsContext.Providerrovider>
+    </foodItemsContext.Provider>
   );
-}
+};
 
 export default App;
